802ae8c6dda21fa9cf397d81a9b5c65adeffd629
rohith <rohith.r.krish@gmail.com>
2021-06-06 17:31:50 +0530

    customer and invites entities

---
+++ reward_system/customer.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module RewardSystem
+  # Customer represents the nodes in the system
+  # Each node having prperties like parent, children and id
+  class Customer
+    attr_reader :name, :id
+    def self.find_or_create(name:)
+      customer = find_by_name(name)
+
+      unless customer
+        customer = new(name: name)
+        REPO.add_customer(customer)
+      end
+      customer
+    end
+
+    def root?
+      Customer.roots.include? self
+    end
+
+    def active?
+      # Either a root or accepted invitation
+      root? || Invite.accepted.any? { |invitation| invitation.friend_id == id }
+    end
+
+    def self.roots
+      # roots are top level customers. They are not recommended by others
+      # Root should have at least one recommendation which is accepted
+
+      REPO.customers.select do |customer|
+        Invite.accepted.none? { |invite| invite.friend_id == customer.id } &&
+          REPO.invites.any? { |invite| invite.customer_id == customer.id }
+      end
+    end
+
+    def parent
+      # Any customer other than root will have an accepted invite.
+      # Invite can be used to identify the invited.
+      return nil if Customer.roots.include? self
+
+      invite = Invite.accepted.find { |inv| inv.friend_id == id }
+      Customer.find_by_id(invite.customer_id)
+    end
+
+    def children
+      # Children are first level recommendations.
+      # Invitations should be in accepted state.
+
+      invites = Invite.accepted.select { |invite| invite.customer_id == id }
+      invites.map { |invite| Customer.find_by_id(invite.friend_id) }
+    end
+
+    private_class_method :new
+
+    def self.find_by_name(name)
+      REPO.customers.find { |customer| customer.name == name }
+    end
+
+    def self.find_by_id(id)
+      REPO.customers.find { |customer| customer.id == id }
+    end
+
+    def initialize(name:)
+      @name = name
+      @id = SecureRandom.uuid
+    end
+  end
+end
---
+++ reward_system/invite.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module RewardSystem
+  # Represents the connection beteween customers.
+  # Considering only accepted invites for building tree structure.
+  class Invite
+    attr_reader :customer_id, :friend_id, :status, :id, :invited_at
+
+    def self.create(customer_id:, friend_id:, status:, invited_at:)
+      REPO.add_invite(new(customer_id, friend_id, status, invited_at))
+    end
+
+    def self.update!(friend:, status:)
+      invite = REPO.invites.find { |inv| inv.friend_id == friend.id }
+      raise Errors::CustomerNotInvited, friend.name unless invite
+
+      invite.update_status(status)
+    end
+
+    def self.accepted
+      # Scope for accepted invites
+
+      REPO.invites.select { |invite| invite.status == 'accepts' }
+    end
+
+    private_class_method :new
+
+    def initialize(customer_id, friend_id, status, invited_at)
+      @customer_id = customer_id
+      @friend_id = friend_id
+      @status = status
+      @id = SecureRandom.uuid
+      @invited_at = invited_at
+    end
+
+    def update_status(status)
+      @status = status
+    end
+  end
+end
